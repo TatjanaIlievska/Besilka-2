1cbb77e0307dccdb88d685d6acae176f79cc30af
Tatjana Ocedova <tatjanaocedova@tatjanas-macbook-pro.local>
2018-09-29 12:58:23 +0200

    Initial Commit

---
+++ Besilka 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Besilka 2
+//
+//  Created by Tatjana Ocedova on 9/29/18.
+//  Copyright © 2018 Tatjana Ocedova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
